0e60c71e166b07017bb5de24d3df4a8e326924a8
Yash Tripathi <tripathiyash97@gmail.com>
2018-03-18 19:21:41 +0530

    Added variable arguments...✌

--- palindrome.c
+++ palindrome.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int main( int argc, char *argv[]){
-	char str[20], *leftside=str, *rightside=str-1;
-	printf("Is it a Palindrome ?\n");
-	scanf("%[^\n]s",str);
-	int isPalindrome=1;
-	while(*(rightside+1)) rightside++;
-	while ((leftside < rightside) && isPalindrome){
-		if (*leftside != *rightside)	isPalindrome=0;
-		leftside++;
-		--rightside;
-	}
-	printf("%s",isPalindrome?"YES, IT IS!":"NOPE, IT AIN'T!");
+	int i, isPalindrome;
+	printf("Is it a palindrome ?\n++++++++++++++++++++++\n\n");
+	for(i=1;i<argc;i++){
+		char *leftside=argv[i], *rightside=argv[i]-1;
+		printf("%s\n",leftside);
+		isPalindrome=1;
+		while(*(rightside+1)) rightside++;
+		while ((leftside < rightside) && isPalindrome){
+			if (*leftside != *rightside)	isPalindrome=0;
+			leftside++;
+			--rightside;
+		}
+		printf("%s",isPalindrome?"YES, IT IS!\n\n":"NOPE, IT AIN'T!\n\n");
+	}	
 	return 0;
 }
